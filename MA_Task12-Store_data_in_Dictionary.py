306f42adfa73db38156756a2f2fe20534ceb2872
Moe <134031268+Mhamadmahdi64@users.noreply.github.com>
2023-06-12 19:05:35 -0400

    store data in Dictionary
    use different kind of print to store data

---
+++ MA_Task12-Store_data_in_Dictionary.py
@@ -0,0 +1,8 @@
+person = {'name':'moe','age':34,'city':'windsor','occu':'it'}
+print(person['name'])
+person['email'] = 'mhamadmahdi64@gmail.com'
+person['age'] = 36
+print(person)
+print(person.keys())
+print(person.values())
+print(person.items())
